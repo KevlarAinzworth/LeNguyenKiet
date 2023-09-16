1e962517bd2ed41a86ff599da43f16b67d4fb225
NguyenKiet <NguyenKiet@DESKTOP-7S7DD11>
2023-09-16 16:01:15 +0700

    Add project files.

---
+++ Controllers/ProductListController.cs
@@ -0,0 +1,38 @@
+﻿using LeNguyenKiet.Models;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Configuration;
+
+namespace LeNguyenKiet.Controllers
+{
+    public class ProductListController : Controller
+    {
+        public static List<ProductListModel> productList = new List<ProductListModel>();
+        public IActionResult ProductList(bool? check = false)
+        {
+            
+            if ((bool)check)
+            {
+                productList.Clear();
+            }
+            else
+            {
+                return View(productList.ToList());
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ProductList(string productName,int productQuantity, int productPrice, double productDiscount)
+        {
+            var homeProducts = new ProductListModel(productName,productQuantity, productPrice, productDiscount);
+            productList.Add(homeProducts);
+            return View(productList);
+        }
+
+
+        public IActionResult RemoveProduct(int position)
+        {
+            productList.RemoveAt(position - 1);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/ProductListModel.cs
@@ -0,0 +1,26 @@
+﻿namespace LeNguyenKiet.Models
+{
+    public class ProductListModel
+    {
+        public string ProductName { get; set; }
+        public double ProductQuantity { get; set; }
+        public double ProductPrice { get; set; }
+        public double ProductDiscount { get; set; }
+        public double DiscountAmount { get; set; }
+        public double TotalBefore { get; set; }
+        public double TotalAfter { get; set; }
+        //int Id { get; set; }
+
+        public ProductListModel() { }
+        public ProductListModel(string name,int quantity, int price, double discount)
+        {
+            this.ProductName = name;
+            this.ProductQuantity = quantity;   
+            this.ProductPrice = price;
+            this.ProductDiscount = discount;
+            this.TotalBefore = price * quantity;
+            this.DiscountAmount = TotalBefore * (discount / 100);
+            this.TotalAfter = TotalBefore - DiscountAmount;
+        }
+    }
+}
